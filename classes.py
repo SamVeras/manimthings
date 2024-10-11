3fab0083e7d0c6afc43fd9d35fa98b957b4dc529
Samuel Veras <samuel.bernardo.veras@gmail.com>
2024-10-11 18:00:14 -0300

    cometnarios

--- classes.py
+++ classes.py
@@ -7,19 +7,30 @@ class RightTriangle(Polygon):
         super().__init__(ORIGIN, side_a * RIGHT, side_b * UP, **kwargs)
 
     def get_side_centers(self, buffer: float = MED_LARGE_BUFF) -> np.ndarray:
+        # As vértices do triângulo, começando do inferior esquerdo, em sentido anti-horário
         v1, v2, v3 = self.get_vertices()
+
+        # O centro de cada aresta do triângulop
         centers = [Line(a, b).get_center() for a, b in zip([v1, v2, v3], [v2, v3, v1])]
 
+        # Função auxiliar pra normalizar um vetor (deixar magnitude = 1)
         def normalize(v):
             return v / np.linalg.norm(v)
 
+        # Função auxiliar para calcular a normal unitária de um vetor definido por dois pontos a e b
+        # Normal = vetor "perpendicular" ao vetor que vai de a -> b
         def unit_normal_helper(a, b):
             v = b - a
             n = np.array([-v[1], v[0], 0])
             return normalize(n)
 
         normals = [unit_normal_helper(a, b) for a, b in zip([v1, v2, v3], [v2, v3, v1])]
+        # Esse produto cruzado é necessário pra determinar se o normal é somado ou subtraído
+        # É necessário caso tenhamos um triângulo flipado, por ex.
+        # Não dá pra entender só aceite
         sign = -1 if (np.cross(v2 - v1, v3 - v1) > 0).any() else 1
+
+        # Ajustar os centros com o buffer
         centers = [center + buffer * normal * sign for center, normal in zip(centers, normals)]
 
         return np.array(centers)
@@ -30,6 +41,7 @@ class RightTriangle(Polygon):
 
     def get_angles(self, right_angle_length=0.4, angle_radius=0.7, color=WHITE) -> VGroup:
         v1, v2, v3 = self.get_vertices()
+        # Não vou comentar essas paradas acho q dá pra entender
         right_angle = RightAngle(Line(v1, v2), Line(v1, v3), length=right_angle_length)
         angle1 = Angle.from_three_points(v1, v2, v3, radius=angle_radius, other_angle=True)
         angle2 = Angle.from_three_points(v2, v3, v1, radius=angle_radius, other_angle=True)
